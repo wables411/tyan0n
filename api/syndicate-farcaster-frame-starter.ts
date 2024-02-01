97bcabd0e63b2a831423c68b0b7371644e5a99c8
GMAn0n <75751135+GMAn0n@users.noreply.github.com>
2024-02-01 03:55:28 -0500

    Update syndicate-farcaster-frame-starter.ts

--- api/syndicate-farcaster-frame-starter.ts
+++ api/syndicate-farcaster-frame-starter.ts
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.20;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenLisa is ERC721 {
     uint256 currentTokenId = 0;
