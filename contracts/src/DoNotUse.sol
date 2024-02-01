fb612bcfbb15e0f0e2c064bd2c02e109e5c88b92
GMAn0n <75751135+GMAn0n@users.noreply.github.com>
2024-02-01 00:37:59 -0500

    Update DoNotUse.sol

--- contracts/src/DoNotUse.sol
+++ contracts/src/DoNotUse.sol
@@ -7,8 +7,10 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract DegenLisa is ERC721 {
     uint256 currentTokenId = 0;
     mapping(address => bool) public hasMinted;
-
-    constructor() ERC721("Framed Nouns", "Framed Nouns") {}
+    address royaltyReceiver; 
+    constructor() ERC721("Degen Mona Lisa", "Degen Mona Lisa") {
+        royaltyReceiver=msg.sender;
+    }
 
     function mint() public {
         require(!hasMinted[msg.sender], "You have already minted an NFT.");
@@ -23,12 +25,12 @@ contract DegenLisa is ERC721 {
         return "https://bafkreif7zy64fask5kl7krraijkkythjn5lcerzkvb3tbwwyryx7jjdcdu.ipfs.nftstorage.link";
     } else if (tokenId % 4 == 0) {
         // Every 5th NFT is some other type
-        return "https://bafkreicwnltaxbqo6hwkqz5ptvizwch3urxu3rvqg7mmtlkyvynuqvh444.ipfs.nftstorage.link/";
+        return "https://bafkreicwnltaxbqo6hwkqz5ptvizwch3urxu3rvqg7mmtlkyvynuqvh444.ipfs.nftstorage.link";
     } else if (tokenId % 3 == 0) {
-        // Every 3rd NFT is yet another type
-        return "https://bafkreieiguvxlgxk6b4nnf7rmxyjoivzsdqroqgu4ubdi6chua7reo3uhu.ipfs.nftstorage.link/";
+        // Every 3rd NFT is Mona Lisa Holding her Hards Close to her Heart
+        return "https://bafkreieiguvxlgxk6b4nnf7rmxyjoivzsdqroqgu4ubdi6chua7reo3uhu.ipfs.nftstorage.link";
     } else {
-        // All other NFTs are Framed Glizzies
+        // All other NFTS are Degen Mona Lisa
         return "https://bafkreiez3updzwhgqexlfx5tsd76b3cmtqoccuhmu4jxpbju6doeg74v6q.ipfs.nftstorage.link";
     }
 }
