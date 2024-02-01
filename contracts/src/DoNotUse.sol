ac3839d86d5938059f6cbaf687d98db264629d39
GMAn0n <75751135+GMAn0n@users.noreply.github.com>
2024-02-01 00:14:35 -0500

    Update DoNotUse.sol

--- contracts/src/DoNotUse.sol
+++ contracts/src/DoNotUse.sol
@@ -2,25 +2,34 @@
 
 pragma solidity ^0.8.20;
 
-import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract OnChainNoun is ERC721 {
+contract DegenLisa is ERC721 {
     uint256 currentTokenId = 0;
+    mapping(address => bool) public hasMinted;
 
     constructor() ERC721("Framed Nouns", "Framed Nouns") {}
 
-    function mint(address to) public {
+    function mint() public {
+        require(!hasMinted[msg.sender], "You have already minted an NFT.");
         ++currentTokenId;
-        _mint(to, currentTokenId);
+        _mint(msg.sender, currentTokenId);
+        hasMinted[msg.sender] = true;
     }
 
-    function tokenURI(uint256 tokenId) public pure override returns (string memory) {
-        // Every 10th NFT is a Unidentified Flying Frame
-        if (tokenId % 10 == 0) {
-            return "https://bafkreicnnfxk7xkykyoq4do42ljrxgb5sstixgsyr3al5yd5yzxm4plh4m.ipfs.nftstorage.link/";
-        } else {
-            // All other NFTs are Framed Glizzies
-            return "https://bafkreih5ewuk5uymaf6sl4b5vivyw6z2ihthpoookwjw2werdmkf5hvgsy.ipfs.nftstorage.link/";
-        }
+ function tokenURI(uint256 tokenId) public pure override returns (string memory) {
+    // Every 10th NFT 
+    if (tokenId % 10 == 0) {
+        return "https://bafkreif7zy64fask5kl7krraijkkythjn5lcerzkvb3tbwwyryx7jjdcdu.ipfs.nftstorage.link";
+    } else if (tokenId % 4 == 0) {
+        // Every 5th NFT is some other type
+        return "https://bafkreicwnltaxbqo6hwkqz5ptvizwch3urxu3rvqg7mmtlkyvynuqvh444.ipfs.nftstorage.link/";
+    } else if (tokenId % 3 == 0) {
+        // Every 3rd NFT is yet another type
+        return "https://bafkreieiguvxlgxk6b4nnf7rmxyjoivzsdqroqgu4ubdi6chua7reo3uhu.ipfs.nftstorage.link/";
+    } else {
+        // All other NFTs are Framed Glizzies
+        return "https://bafkreiez3updzwhgqexlfx5tsd76b3cmtqoccuhmu4jxpbju6doeg74v6q.ipfs.nftstorage.link";
     }
+}
 }
